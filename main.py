a19d5bab246e020e5a6220550bd2a26c8718c8ff
ram <ccvy.ram@gmail.com>
2022-07-20 10:51:31 +0530

    version 1

---
+++ main.py
@@ -0,0 +1,16 @@
+
+# Press the green button in the gutter to run the script.
+
+def main():
+    a=int(input('enter a:'))
+    b=int(input('enter b:'))
+    print(a+b)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
